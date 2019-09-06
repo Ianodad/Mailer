6db35464de9c0864c612c8e3cbd5c87426a63c19
Ian Odhiambo <you@example.com>
2019-09-06 14:35:23 +0300

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 var exphbs = require('express-handlebars');
 
 const app = express();
+const helmet = require('helmet');
+const compression = require('compression');
+
+
+
+
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -9,6 +15,9 @@ app.set('view engine', 'handlebars');
 app.use('/', require('./routes/home'))
 app.use('/view', require('./routes/view'))
 
+// production 
+app.use(helmet());
+app.use(compression())
 
 const PORT = process.env.PORT || 8000;
 
